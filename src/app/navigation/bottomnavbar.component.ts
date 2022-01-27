e3fe112e1ee90dc2c9d23cd9238727c3b55f078f
jaa0054 <jaa0054@mix.wvu.edu>
2022-01-26 22:45:16 -0500

    added bottomnavbar component

---
+++ src/app/navigation/bottomnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'strava-bottomnavbar',
+    templateUrl: 'bottomnavbar.component.html',
+    styleUrls: ['bottomnavbar.component.css']
+})
+export class BottomNavBarComponent{
+    
+}
