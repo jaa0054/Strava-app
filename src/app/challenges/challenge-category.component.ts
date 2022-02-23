678007dbea0f64314e45ac67db89592c38710b42
jack-desktop <jaa0054@mix.wvu.edu>
2022-02-23 14:46:51 -0500

    added categories to the challenges page

---
+++ src/app/challenges/challenge-category.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'strava-challenge-category',
+    templateUrl: 'challenge-category.component.html',
+    styleUrls: ['challenge-category.component.css']
+})
+export class ChallengeCatagoryComponent{
+   
+}
--- src/app/challenges/challenge-layout.component.ts
+++ src/app/challenges/challenge-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 
+
 @Component({
     selector: 'strava-challenge-layout',
     templateUrl: 'challenge-layout.component.html'
 })
 export class ChallengeLayoutComponent{
-
+    
 }
