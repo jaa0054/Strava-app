919ad8cd8dbd4b0092a315d8508bef955889cd80
jack-desktop <jaa0054@mix.wvu.edu>
2022-02-23 12:03:48 -0500

    activity and challenge cards for dashboard moved to dashboard, and flex layout used to move items around

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivityModel } from './dashboard/activity-item.model';
-import { ChallengeModel } from './dashboard/challenge-item.model';
-import { challenge_list } from './dashboard/challenge-list';
-import { activity_list } from './dashboard/activity-list';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +7,7 @@ import { activity_list } from './dashboard/activity-list';
 })
 export class AppComponent {
   title = 'DashBoard | Strava';
-  challenges: ChallengeModel [] = [];
-  activities: ActivityModel [] = [];
+  
 
-  constructor() {
-    for (var challenge of challenge_list) {
-      this.challenges.push(challenge);
-    }
-    for (var activity of activity_list) {
-      this.activities.push(activity);
-    }
-    }
+ 
     }
--- src/app/dashboard/dashboard-layout.component.ts
+++ src/app/dashboard/dashboard-layout.component.ts
@@ -1,10 +1,23 @@
 import { Component } from "@angular/core";
+import { ActivityModel } from "./activity-item.model";
 import { activity_list } from "./activity-list";
+import { ChallengeModel } from "./challenge-item.model";
+import { challenge_list } from "./challenge-list";
 
 @Component({
     selector: 'strava-dashboard-layout',
     templateUrl: 'dashboard-layout.component.html'
 })
 export class DashboardLayoutComponent{
-    activity_list: any = activity_list;
+    challenges: ChallengeModel [] = [];
+    activities: ActivityModel [] = [];
+
+    constructor() {
+        for (var challenge of challenge_list) {
+          this.challenges.push(challenge);
+        }
+        for (var activity of activity_list) {
+          this.activities.push(activity);
+        }
+        }
 }
